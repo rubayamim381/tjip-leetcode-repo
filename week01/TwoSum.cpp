e2c4d02abbc95da368fcf130b3784b4dee55e4e9
Rubaya Akter Mim <45107866+rubayamim381@users.noreply.github.com>
2022-12-09 13:41:37 +0600

    two sums

--- week01/TwoSum.cpp
+++ week01/TwoSum.cpp
@@ -3,14 +3,15 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
         unordered_map<int,int> link;
+        
 
         for(int i=0; i<nums.size(); i++)
             link.insert(pair<int, int>(nums[i], i));
 
         for(int index = 0; index < nums.size(); index++){
-
-            if(link.find(target-nums[index]) != link.end() && link[target-nums[index]] != index)
-                return {index, link[target-nums[index]]};
+            int target_num=target-nums[index];
+            if(link.find(target_num) != link.end() && link[target_num] != index)
+                return { index, link[target_num] };
 
         }
 
